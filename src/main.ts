6ec295b83d0dd63e23412e5f4400fb08d6f70f62
xuheng <ob12564@163.com>
2023-05-11 17:42:11 +0800

    nothing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as session from 'express-session';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(session());
   await app.listen(3000);
 }
 bootstrap();
